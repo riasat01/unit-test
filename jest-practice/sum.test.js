e0b7ddad6b5a3cd10450ff09ced1017e09b462c1
riasat01 <riasat01@outlook.com>
2024-03-25 01:17:28 +0600

    some more test added

--- jest-practice/sum.test.js
+++ jest-practice/sum.test.js
@@ -4,3 +4,13 @@ const { sum } = sumObject;
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
 })
+
+test('object assignment', () => {
+    const data = { one: 1 };
+    data['two'] = 2;
+    expect(data).toEqual({ one: 1, two: 2 });
+})
+
+test('null is falsy', () => {
+    expect(null).toBeFalsy();
+})
